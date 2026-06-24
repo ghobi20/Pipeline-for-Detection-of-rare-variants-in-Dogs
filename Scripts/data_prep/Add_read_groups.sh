8b02df38f45b1ea7f0bf020e18640ecbc2ebfd59
ghobi20 <ghobibohg@gmail.com>
2026-06-24 14:16:36 -0600

    Create SLURM script for adding read groups to BAM files
    This script sets up a SLURM job to add read groups to BAM files using Picard and Samtools.

---
+++ Scripts/data_prep/Add_read_groups.sh
@@ -0,0 +1,34 @@
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=32G
+#SBATCH --array=1-4
+#SBATCH --output=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/logs/Add_read_groups_%A_%a.out
+#SBATCH --error=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/logs/Add_read_groups_%A_%a.err
+#SBATCH --chdir=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/output/bam_output
+
+module purge
+
+# Carga tus modulos en la siguiente linea
+
+module load picard/2.6.0
+module load samtools/1.22.1
+
+# Your script goes here
+
+Dog_ID="P${SLURM_ARRAY_TASK_ID}"
+
+echo "Procesando la muestra ${Dog_ID} en la tarea ${SLURM_ARRAY_TASK_ID}" 
+
+#Lo pongo ahora porque no sabía que existía la opción -R en bwa -mem. Los read groups son necesarios para introducirlos al pipe de gatk. La info ffue obtenida de las reads crudas.
+picard AddOrReplaceReadGroups \
+R=/mnt/data/cgonzaga/Data/Dog_genomes/reference/canFam6.fa.gz \
+I=${Dog_ID}.alignment.sort.dedup.bam \
+O=${Dog_ID}.alignment.sort.dedup.rg.bam \
+RGLB=lib1 \
+RGPL=illumina \
+RGPU=22JCHNLT4.8.${Dog_ID} \
+RGSM=${Dog_ID} \
+RGID=22JCHNLT4.8
+
+echo "Creando nuevo indice para bam con Read Groups (RG)"
+
