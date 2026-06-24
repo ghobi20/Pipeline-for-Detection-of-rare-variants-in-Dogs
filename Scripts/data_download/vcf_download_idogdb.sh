6010bd86d2a29f6e23486e7aba7fcc394a566d93
ghobi20 <ghobibohg@gmail.com>
2026-06-24 14:12:41 -0600

    Add download commands for all_SNP.vcf files
    In the fenix cluster this file is divided in two parts, one of INDELs and one of SNPs. I did so to speed up the download time, but its irrelevant.

---
+++ Scripts/data_download/vcf_download_idogdb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=vcf_INDEL_variants_10kDog
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=5G 
+#SBATCH --output=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/logs/vcf_INDEL_variants_10kDog_%A_%a.out
+#SBATCH --error=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/logs/vcf_INDEL_variants_10kDog_%A_%a.err
+#SBATCH --chdir=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/resources/known_variants/
+
+module purge
+
+# Carga tus modulos en la siguiente linea
+wget -r ftp://download.big.ac.cn/idog/dogsd/vcf/canfam4/indel/AutoAndXPAR.nonSNPs.filter.vcf.gz
+wget -r ftp://download.big.ac.cn/idog/dogsd/vcf/canfam4/indel/AutoAndXPAR.nonSNPs.filter.vcf.gz.tbi
+wget -r ftp://download.big.ac.cn/idog/dogsd/vcf/canfam4/all_SNP.vcf.gz
+wget -r ftp://download.big.ac.cn/idog/dogsd/vcf/canfam4/all_SNP.vcf.gz.tbi
+
+
