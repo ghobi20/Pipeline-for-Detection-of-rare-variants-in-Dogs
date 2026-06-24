f55c3acf2a66936f81fdaa5fce925736f6fa6e78
ghobi20 <ghobibohg@gmail.com>
2026-06-24 13:13:41 -0600

    Annotation database resources download
    First draft. Uploading straight from the cluster

---
+++ Scripts/Ann_database_download.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=liftOver_canFam4_to_canFam6_map
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=5G
+#SBATCH --output=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/logs/canfam4_to_canfam6_%A_%a.out
+#SBATCH --error=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/logs/canfam4_to_canfam6_%A_%a.err
+#SBATCH --chdir=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/resources
+
+module purge
+
+# Carga tus modulos en la siguiente linea
+wget -r ftp://hgdownload.soe.ucsc.edu/goldenPath/canFam6/database/refGene.txt.gz 
+wget -r ftp://hgdownload.soe.ucsc.edu/goldenPath/canFam6/database/refLink.txt.gz 
+wget -r ftp://hgdownload.soe.ucsc.edu/goldenPath/canFam6/database/refGene.txt.gz 
