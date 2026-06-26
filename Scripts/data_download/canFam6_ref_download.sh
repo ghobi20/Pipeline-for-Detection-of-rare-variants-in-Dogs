46c6beb758ee2ad95115d9116fcbc672d2d16adc
ghobi20 <ghobibohg@gmail.com>
2026-06-25 19:21:58 -0600

    Create canFam6_ref_download.sh for genome download
    Add a SLURM script for downloading the canFam6 reference genome.

---
+++ Scripts/data_download/canFam6_ref_download.sh
@@ -0,0 +1,19 @@
+  GNU nano 5.6.1                                                                      canFam6_ref_download.sh                                                                                 
+#!/bin/bash
+
+#SBATCH --job-name=canFam6_ref_download
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=32G
+#SBATCH --output=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/logs/canFam6_ref_download.out
+#SBATCH --error=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/logs/canFam6_ref_download.err
+#SBATCH --chdir=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/resources/reference/canFam6
+
+module purge
+
+# Carga tus modulos en la siguiente linea
+
+# Your script goes here
+
+wget --timestamping 'ftp://hgdownload.gi.ucsc.edu/goldenPath/canFam6/bigZips/canFam6.fa.gz'
