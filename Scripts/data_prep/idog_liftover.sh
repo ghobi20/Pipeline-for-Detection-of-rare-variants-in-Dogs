76887c88d3231767faf7bdd300b4ecbd76beef40
ghobi20 <ghobibohg@gmail.com>
2026-06-25 19:44:40 -0600

    Update reference genome path in idog_liftover.sh

--- Scripts/data_prep/idog_liftover.sh
+++ Scripts/data_prep/idog_liftover.sh
@@ -23,7 +23,7 @@ picard LiftoverVcf \
   O=../canFam6/all_SNP_canFam6.vcf.gz \
   CHAIN=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/resources/liftOver_info/chainmap/canFam4ToCanFam6.over.chain.gz \
   REJECT=all_SNP_rejected.vcf.gz \
-  R=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/resources/canFam6/canFam6.fa.gz
+  R=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/resources/canFam6/reference/canFam6.fa.gz
 
 echo "Transformando el vcf a bed de los INDELs de iDOG..."
 picard LiftoverVcf \
@@ -31,4 +31,4 @@ picard LiftoverVcf \
   O=../canFam6/AutoAndXPAR.nonSNPs.filter.vcf.gz \
   CHAIN=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/resources/liftOver_info/chainmap/canFam4ToCanFam6.over.chain.gz \
   REJECT=AutoAndXPAR.nonSNPs.filter.rejected.vcf.gz \
-  R=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/resources/canFam6/canFam6.fa.gz
+  R=/mnt/data/cgonzaga/sgamino/Dog_epilepsy_project/resources/canFam6/reference/canFam6.fa.gz
